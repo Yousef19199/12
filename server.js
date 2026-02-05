86855833a0b30693fe95ddd1fdf0874b3491aa7d
Yousef19199 <yykama2@gmail.com>
2026-02-05 21:08:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -135,7 +135,11 @@ client.on('messageCreate', message => {
 });
 client.login(process.env.TOKEN);
 
-app.listen(3000, () => console.log('✅ السيرفر والويب شغال على http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
+
 
 
 
